5c859500ab46e47a82966a2c253c81b6375f17a7
Jiggly Puff <mian.wang.eit@gmail.com>
2021-11-15 05:43:38 -0800

    Add files via upload

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { FlatList, ScrollView } from "react-native-gesture-handler";
 import { Card } from "react-native-elements";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, ScrollView } from "react-native";
 import { ListItem } from "react-native-elements";
 import { PARTNERS } from "../shared/partners";
 
