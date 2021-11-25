fb8f6a304c78faa3acb21e2fdda4847245e4887d
Jiggly Puff <mian.wang.eit@gmail.com>
2021-11-25 04:43:31 -0800

    Add files via upload

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import { Card } from "react-native-elements";
 import { Text } from "react-native";
+import * as Animatable from 'react-native-animatable';
 
 
 class Contact extends Component {
@@ -12,15 +13,17 @@ class Contact extends Component {
     render(){
         return (
             <ScrollView>
-                <Card title="Contact Information" wrapperStyle={{margin: 20}}>
-                
-                    <Text >1 Nucamp Way</Text>
-                    <Text >Seattle, WA 98001</Text>
-                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
-                    <Text >Phone: 1-206-555-1234</Text>
-                    <Text >Email: campsites@nucamp.co</Text>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card title="Contact Information" wrapperStyle={{margin: 20}}>
+                    
+                        <Text >1 Nucamp Way</Text>
+                        <Text >Seattle, WA 98001</Text>
+                        <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                        <Text >Phone: 1-206-555-1234</Text>
+                        <Text >Email: campsites@nucamp.co</Text>
 
-                </Card>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         
         );
